bfee62ca92bc673848b1a4eea6f5b27f1577b58a
Git Demo <CICDDemo2020@outlook.com>
2021-09-09 14:45:45 +0530

    Fifth commit

--- src/test/java/com/stepDefinition/LoginSteps.java
+++ src/test/java/com/stepDefinition/LoginSteps.java
@@ -21,7 +21,7 @@ public class LoginSteps {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://demoqa.com/login");
+		//driver.get("https://demoqa.com/login");
 	}
 
 	@When("User enters UserName and Password")
